2dffe105a29c506fecf803e3c644c5d6a5f38173
Sebastian Schwarz <sbstnschwrz@gmail.com>
2013-01-22 21:01:42 +0100

    Erstellung der Application.java mit Main-Methode und Package "project.main". Sämtliche folgende Packages bitte mit "project.XY" beginnen!

---
+++ src/project/main/Application.java
@@ -0,0 +1,16 @@
+package project.main;
+
+/**
+ * @author Schwarz,Keck,Matejka,Rößler
+ * @version IN DEVELOPMENT
+ *
+ */
+
+public class Application
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Debugging");
+	}
+
+}
