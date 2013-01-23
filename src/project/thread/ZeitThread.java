72e18cc0a9a0113e2fc59d4e5c513acb12b96001
Leander <Leander@Leander-HP>
2013-01-23 20:27:09 +0100

    public void run()-Methode prog.

--- src/project/thread/ZeitThread.java
+++ src/project/thread/ZeitThread.java
@@ -7,4 +7,20 @@ public class ZeitThread extends Thread
 	{
 		_zeit = zeit;
 	}
+	public void run()
+	{
+		while(_zeit > 0)
+		{
+			_zeit = _zeit -1;
+			try 
+			{
+				sleep(1000);
+			} 
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
