af08cd588e4c170ed430fc8f92d1f3186a435e2b
Leander <Leander@Leander-HP>
2013-01-23 20:48:47 +0100

    Konstruktor erweitert + Methode setCountdown erstellt

--- src/project/window/Abschreibfrm.java
+++ src/project/window/Abschreibfrm.java
@@ -1,6 +1,19 @@
 package project.window;
 
-public class Abschreibfrm
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Abschreibfrm extends JFrame
 {
-	
+	private int zeit = 0;
+	private JLabel lblcountdown;
+	public Abschreibfrm()
+	{
+		super("Abschreiben");
+		lblcountdown = new JLabel();
+	}
+	public void setCountdown(String text)
+	{
+		lblcountdown.setText("Zeit:"+text);
+	}
 }
