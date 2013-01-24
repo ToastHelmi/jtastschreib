18684341ea03072ba91605eb6442c3ec7e906507
ToastHelmi <lukas-ma@web.de>
2013-01-24 12:53:39 +0100

    add serial

--- src/project/Exception/DecryptException.java
+++ src/project/Exception/DecryptException.java
@@ -2,6 +2,11 @@ package project.Exception;
 
 public class DecryptException extends Exception 
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2134045978092114365L;
+
 	public DecryptException(String nachricht)
 	{
 		super(nachricht);
