885cc87098125a55a1181d9c856e7cac598035dd
ToastHelmi <lukas-ma@web.de>
2013-01-24 15:30:53 +0100

    Add Textdatei.java um datein einzulesen

---
+++ src/project/text/TextDatei.java
@@ -0,0 +1,48 @@
+package project.text;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextDatei 
+{
+	public static String getCryptedText(String path)
+	{	
+		 String s = null,result = "";
+		 BufferedReader br = null;
+		 try 
+		 {
+			br = new BufferedReader(new FileReader(path));
+			while((s = br.readLine()) != null)
+			{
+				result += s;
+			}
+		} catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(br != null)
+			{
+				try 
+				{
+					br.close();
+				} 
+				catch (IOException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		 return result;
+	}
+
+}
