6d58537a345b277df3c43c3483ddc910c403da17
4DLeo <leo.lll@gmx.de>
2013-01-24 18:33:25 +0100

    Aussehen auf Nimbus gesetzt

--- src/project/main/Application.java
+++ src/project/main/Application.java
@@ -1,5 +1,8 @@
 package project.main;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 import project.window.MainWindow;
 
 /**
@@ -13,6 +16,22 @@ public class Application
 	public static void main(String[] args)
 	{
 		System.out.println("Debugging");
+		//Setzt das Aussehen auf Nimbus(falls vorhanden)
+		try 
+		{
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) 
+		    {
+		        if ("Nimbus".equals(info.getName()))
+		        {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} 
+		catch (Exception e) 
+		{
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		MainWindow w = new MainWindow(); 
 		w.setVisible(true);//Das Auswahlfenster wird erzeugt und angezeigt
 	}
