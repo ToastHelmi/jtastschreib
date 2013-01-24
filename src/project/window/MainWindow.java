448d214ae78e4c66b53b4af220c3d2268402bf53
4DLeo <leo.lll@gmx.de>
2013-01-24 18:28:42 +0100

    Weiteres Element hinzugefügt. + ButtonKlick-Methode die aufgerufen wird wenn einer der beiden Buttons geklickt wurde.

--- src/project/window/MainWindow.java
+++ src/project/window/MainWindow.java
@@ -2,15 +2,18 @@ package project.window;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JSpinner;
 
 public class MainWindow extends JFrame 
 {
 	//MainWindow = AuswahlWindow
 	private static final long serialVersionUID = -2433186056785629618L;
-	
+	private JSpinner minuten;
 	//Standardkonstruktor
 	public MainWindow()
 	{
@@ -18,18 +21,48 @@ public class MainWindow extends JFrame
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Beendet alles wenn dieses Fenster geschlossen wird
 		this.setLayout(new GridBagLayout());//Setzt das Layout auf das GridBagLayout
 		JButton abschreiben = new JButton("Abschreiben auf Zeit");//Erzeugt einen neuen Auswahlbutton
+		abschreiben.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				ButtonKlick(arg0);
+			}
+		});
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
 		this.add(abschreiben,c);
 		
 		JButton lernen = new JButton("Lernen");
+		lernen.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				ButtonKlick(e);
+			}
+		});
 		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 1;
 		this.add(lernen,c);
 		
-		
+		minuten = new JSpinner();
+		c = new GridBagConstraints();
+		c.gridx = 1;
+		c.gridy = 0;
+		this.add(minuten,c);
 	}
-
+	public void ButtonKlick(ActionEvent e)
+	{
+		if(e.getActionCommand().equals("Abschreiben auf Zeit"))
+		{
+			Abschreibfrm f = new Abschreibfrm((int)minuten.getValue());
+		}
+		else if(e.getActionCommand().equals("Lernen"))
+		{
+			//Lernenfrm f = new Lernenfrm();
+		}
+	}
 }
