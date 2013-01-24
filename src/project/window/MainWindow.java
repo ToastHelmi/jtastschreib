4d6e90dacbe89f422d9ff76f46ac81e2d8f5ee36
4DLeo <leo.lll@gmx.de>
2013-01-24 18:18:08 +0100

    GUI-Elemente hinzugefügt

--- src/project/window/MainWindow.java
+++ src/project/window/MainWindow.java
@@ -1,5 +1,9 @@
 package project.window;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame 
@@ -11,6 +15,21 @@ public class MainWindow extends JFrame
 	public MainWindow()
 	{
 		//Total toll
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);//Beendet alles wenn dieses Fenster geschlossen wird
+		this.setLayout(new GridBagLayout());//Setzt das Layout auf das GridBagLayout
+		JButton abschreiben = new JButton("Abschreiben auf Zeit");//Erzeugt einen neuen Auswahlbutton
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		this.add(abschreiben,c);
+		
+		JButton lernen = new JButton("Lernen");
+		c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 1;
+		this.add(lernen,c);
+		
+		
 	}
 
 }
