5ba742eed8fe6030be03db6329e2920ee5e97611
Lukas <lukas-ma@web.de>
2013-01-26 11:49:26 +0100

    add Eingebetext

---
+++ src/project/text/Eingabetext.java
@@ -0,0 +1,27 @@
+package project.text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Eingabetext 
+{
+	private List<String> list;
+	
+	private Eingabetext()
+	{
+		list  = new ArrayList<String>();
+	}
+	public void add(String text)
+	{
+		list.add(text);
+	}
+	public String[] getEingabe()
+	{
+		return (String[])list.toArray();
+	}
+	public int length()
+	{
+		return list.size();
+	}
+
+}
