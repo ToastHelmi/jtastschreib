df955c6a8ed99748b4cf96b86cee5bdccad916a7
4DLeo <leo.lll@gmx.de>
2013-01-27 15:49:53 +0100

    Alle Tasten sind in der Klasse Taste

--- src/project/window/components/leuchtpanel/Leuchtpanel.java
+++ src/project/window/components/leuchtpanel/Leuchtpanel.java
@@ -6,6 +6,7 @@ import java.awt.Graphics;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -14,22 +15,27 @@ public class Leuchtpanel extends JPanel
 	private Font _font = new Font("Arial",Font.PLAIN,20);
 	private FontRenderContext _frc =  new FontRenderContext(new AffineTransform(),false,false);
 	private Rectangle2D _fontsize;
-	public final static String[] Tasten = {"^","1","2","3","4","5","6","7","8","9","0","?","`"};
-	public Leuchtpanel()
+	private ArrayList<Taste> _tasten;
+	public Leuchtpanel(ArrayList<Taste> tasten)
 	{
+		_tasten = tasten;
 		this.setSize(new Dimension(600,300));
 		this.setFont(_font);
 	}
 	public void paintComponent(Graphics g)
 	{
-		for(int i = 0; i < Tasten.length;i++)
+//		for(int i = 0; i < Tasten.length;i++)
+//		{
+//			drawTaste(Color.PINK,Tasten[i],(i*50),0 ,45,45,g); 
+//		}
+//		drawTaste(Color.GRAY,"Backspace",(Tasten.length * 50), 0 , 120,45,g);
+//		for(int i = 0; i < Tasten.length;i++)
+//		{
+//			drawTaste(Color.GREEN,Tasten[i],(i*50)+22,50,45,45,g);
+//		}
+		for(Taste t : _tasten)
 		{
-			drawTaste(Color.PINK,Tasten[i],(i*50),0 ,45,45,g); 
-		}
-		drawTaste(Color.GRAY,"Backspace",(Tasten.length * 50), 0 , 120,45,g);
-		for(int i = 0; i < Tasten.length;i++)
-		{
-			drawTaste(Color.GREEN,Tasten[i],(i*50)+22,50,45,45,g);
+			drawTaste(t.getFarbe(),t.getBeschriftung(),t.getXstelle()*50,t.getYstelle()*50,t.getWidth(),t.getHeight(),g);
 		}
 	}
 	private void drawTaste(Color c, String text, int xPos, int yPos, int width, int height, Graphics g)
