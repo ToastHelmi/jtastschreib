d92767f052907c4d91966bba354fd8ed96c07cca
4DLeo <leo.lll@gmx.de>
2013-01-27 20:52:20 +0100

    ZeitFrm erbt nun nicht mehr von JFrame sondern von JDialog(wegen this.setModal(true))

--- src/project/window/ZeitFrm.java
+++ src/project/window/ZeitFrm.java
@@ -1,9 +1,10 @@
 package project.window;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public abstract class ZeitFrm extends JFrame
+public abstract class ZeitFrm extends JDialog
 {
 	private JLabel lblcountdown = new JLabel(); //sorgt dafür das Fenster die davon abgeleitet sind über den ZeitThread die restliche Zeit bekommen
 	public ZeitFrm(String text)
