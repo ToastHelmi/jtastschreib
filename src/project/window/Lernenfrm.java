e676eb18f498ab22d71357db653a63f008d44d1c
4DLeo <leo.lll@gmx.de>
2013-01-27 16:02:23 +0100

    LeuchtPanel hinzugefügt + ScrollPane für das Eingabefeld

--- src/project/window/Lernenfrm.java
+++ src/project/window/Lernenfrm.java
@@ -2,18 +2,23 @@ package project.window;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.ScrollPane;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 import project.text.Vorgabetext;
 import project.window.components.eingabefeld.Eingabefeld;
+import project.window.components.leuchtpanel.Leuchtpanel;
+import project.window.components.leuchtpanel.Taste;
 
 public class Lernenfrm extends ZeitFrm
 {
 	private Vorgabetext vorgabe;
-	//private Leuchttastenpanel _panel;
+	private Leuchtpanel _panel;
 	private Eingabefeld _eingabe;
 	public Lernenfrm(Vorgabetext vorgabetext)
 	{
@@ -24,20 +29,25 @@ public class Lernenfrm extends ZeitFrm
 	public void InitializeComponent()
 	{
 		//Erstellt alle Komponenten
-		//_panel = new Leuchttastenpanel();
+		_panel = new Leuchtpanel(Taste.getNormalKeyboard());
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
 		c.weightx = 1;
-		//this.add(_panel,c);
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		this.add(_panel,c);
 		
 		_eingabe = new Eingabefeld(true);
 		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 1;
 		c.weightx = 1;
-		this.add(_eingabe,c);
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		JScrollPane _pane = new JScrollPane(_eingabe,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		this.add(_pane,c);
 		this.pack();
 	}
 	@Override
